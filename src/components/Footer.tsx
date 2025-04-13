3b95b738f7e75fa1a03a54fc59eb095f768c1e6d
yoel3imari <youssef.elaimari@gmail.com>
2025-04-13 19:19:28 +0100

    fix module import

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-8 p-4 bg-secondary">
+      <h3 className="text-center">yoel3imari &copy; {new Date().getFullYear()}</h3>
+    </footer>
+  );
+}
